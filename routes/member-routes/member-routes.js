8fe1475a886ebe1526ff35e5c53672c255914561
Developer's Community - NMP <161813137+DevCommNMP@users.noreply.github.com>
2024-09-09 12:06:11 +0530

    Update member-routes.js

--- routes/member-routes/member-routes.js
+++ routes/member-routes/member-routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { createMember } = require('../../controllers/member-controller/member-controller');
+const { createMember,getMembers } = require('../../controllers/member-controller/member-controller');
 
 // Route to create a new member
 router.post('/members', createMember);
-
+router.get('/getmembers',getMembers)
 module.exports = router;
